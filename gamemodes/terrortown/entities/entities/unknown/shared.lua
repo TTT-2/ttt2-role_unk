283f203c60ad6df323b0653a0bd91128fdef120f
Alf21 <4lf-mueller@gmx.de>
2018-10-25 01:00:40 +0200

    Compatibility with TTT2 v0.3.0b + Sidekick and Infected fix

--- gamemodes/terrortown/entities/entities/unknown/shared.lua
+++ gamemodes/terrortown/entities/entities/unknown/shared.lua
@@ -7,7 +7,7 @@ end
 
 -- important to add roles with this function,
 -- because it does more than just access the array ! e.g. updating other arrays
-InitCustomRole("UNKNOWN", { -- first param is access for ROLES array => ROLES.UNKNOWN or ROLES["UNKNOWN"]
+InitCustomRole("UNKNOWN", { -- first param is access for ROLES array => UNKNOWN or ROLES["UNKNOWN"]
 		color = Color(0, 0, 0, 255), -- ...
 		dkcolor = Color(50, 50, 50, 255), -- ...
 		bgcolor = Color(50, 50, 50, 200), -- ...
@@ -27,65 +27,67 @@ InitCustomRole("UNKNOWN", { -- first param is access for ROLES array => ROLES.UN
 		random = 10 -- randomness of getting this role selected in a round
 })
 
--- if sync of roles has finished
-hook.Add("TTT2_FinishedSync", "UnkInitT", function(ply, first)
-	if CLIENT and first then -- just on client and first init !
-		infMat = Material("vgui/ttt/sprite_" .. ROLES.UNKNOWN.abbr)
+if CLIENT then -- just on client and first init !
+	-- if sync of roles has finished
+	hook.Add("TTT2FinishedLoading", "UnkInitT", function()
+		infMat = Material("vgui/ttt/sprite_" .. UNKNOWN.abbr)
 
 		-- setup here is not necessary but if you want to access the role data, you need to start here
 		-- setup basic translation !
-		LANG.AddToLanguage("English", ROLES.UNKNOWN.name, "Unknown")
-		LANG.AddToLanguage("English", "info_popup_" .. ROLES.UNKNOWN.name, [[You can decide who you want to be...]])
-		LANG.AddToLanguage("English", "body_found_" .. ROLES.UNKNOWN.abbr, "This was an Unknown...")
-		LANG.AddToLanguage("English", "search_role_" .. ROLES.UNKNOWN.abbr, "This person was an Unknown!")
-		LANG.AddToLanguage("English", "target_" .. ROLES.UNKNOWN.name, "Unknown")
-		LANG.AddToLanguage("English", "ttt2_desc_" .. ROLES.UNKNOWN.name, [[The Unknown can decide (more or less) the role.
+		LANG.AddToLanguage("English", UNKNOWN.name, "Unknown")
+		LANG.AddToLanguage("English", "info_popup_" .. UNKNOWN.name, [[You can decide who you want to be...]])
+		LANG.AddToLanguage("English", "body_found_" .. UNKNOWN.abbr, "This was an Unknown...")
+		LANG.AddToLanguage("English", "search_role_" .. UNKNOWN.abbr, "This person was an Unknown!")
+		LANG.AddToLanguage("English", "target_" .. UNKNOWN.name, "Unknown")
+		LANG.AddToLanguage("English", "ttt2_desc_" .. UNKNOWN.name, [[The Unknown can decide (more or less) the role.
 Get killed by someone to copy the role of your killer!]])
-
-		-- optional for toggling whether player can avoid the role
-		LANG.AddToLanguage("English", "set_avoid_" .. ROLES.UNKNOWN.abbr, "Avoid being selected as Unknown!")
-		LANG.AddToLanguage("English", "set_avoid_" .. ROLES.UNKNOWN.abbr .. "_tip",
-		[[Enable this to ask the server not to select you as Unknown if possible. Does not mean you are Traitor more often.]])
 
 		---------------------------------
 
 		-- maybe this language as well...
-		LANG.AddToLanguage("Deutsch", ROLES.UNKNOWN.name, "Unbekannter")
-		LANG.AddToLanguage("Deutsch", "info_popup_" .. ROLES.UNKNOWN.name, [[Du hast die Wahl, wer du sein willst...]])
-		LANG.AddToLanguage("Deutsch", "body_found_" .. ROLES.UNKNOWN.abbr, "Er war ein Unbekannter...")
-		LANG.AddToLanguage("Deutsch", "search_role_" .. ROLES.UNKNOWN.abbr, "Diese Person war ein Unbekannter!")
-		LANG.AddToLanguage("Deutsch", "target_" .. ROLES.UNKNOWN.name, "Unbekannter")
-		LANG.AddToLanguage("Deutsch", "ttt2_desc_" .. ROLES.UNKNOWN.name, [[Die Unbekannten können sich ihre Rolle (mehr oder weniger) aussuchen.
+		LANG.AddToLanguage("Deutsch", UNKNOWN.name, "Unbekannter")
+		LANG.AddToLanguage("Deutsch", "info_popup_" .. UNKNOWN.name, [[Du hast die Wahl, wer du sein willst...]])
+		LANG.AddToLanguage("Deutsch", "body_found_" .. UNKNOWN.abbr, "Er war ein Unbekannter...")
+		LANG.AddToLanguage("Deutsch", "search_role_" .. UNKNOWN.abbr, "Diese Person war ein Unbekannter!")
+		LANG.AddToLanguage("Deutsch", "target_" .. UNKNOWN.name, "Unbekannter")
+		LANG.AddToLanguage("Deutsch", "ttt2_desc_" .. UNKNOWN.name, [[Die Unbekannten können sich ihre Rolle (mehr oder weniger) aussuchen.
 Werde von jemanden getötet, um dessen Rolle zu kopieren.]])
-
-		LANG.AddToLanguage("Deutsch", "set_avoid_" .. ROLES.UNKNOWN.abbr, "Vermeide als Unbekannter ausgewählt zu werden!")
-		LANG.AddToLanguage("Deutsch", "set_avoid_" .. ROLES.UNKNOWN.abbr .. "_tip",
-		[[Aktivieren, um beim Server anzufragen, nicht als Unbekannter ausgewählt zu werden. Das bedeuted nicht, dass du öfter Traitor wirst!]])
-	end
-end)
+	end)
+end
 
 if SERVER then
 	hook.Add("PlayerDeath", "UnknownDeath", function(victim, infl, attacker)
-		if victim:GetRole() == ROLES.UNKNOWN.index and IsValid(attacker) and attacker:IsPlayer() and attacker:GetRole() ~= ROLES.UNKNOWN.index then
+		if victim:GetSubRole() == ROLE_UNKNOWN and IsValid(attacker) and attacker:IsPlayer() and attacker:GetSubRole() ~= ROLE_UNKNOWN then
+			if INFECTED and attacker:GetSubRole() == ROLE_INFECTED then return end
+
 			victim.unknownKiller = attacker
 		end
 	end)
 
 	hook.Add("PostPlayerDeath", "UnknownPostDeath", function(ply)
-		if ply:GetRole() == ROLES.UNKNOWN.index and IsValid(ply.unknownKiller) then
-			timer.Simple(1, function() -- respawn delay
-				ply:SetRole(ROLES.INNOCENT.index)
-				ply:SpawnForRound(true)
+		if ply:GetSubRole() == ROLE_UNKNOWN then
+			local killer = ply.unknownKiller
 
-				local killer = ply.unknownKiller
+			ply.unknownKiller = nil
 
-				ply.unknownKiller = nil
+			if IsValid(killer) then
 
-				ply:UpdateRole(killer:GetRole())
-				ply:SetDefaultCredits()
+				-- revive after 3s
+				ply:Revive(3, function(p)
+					if IsValid(p) and IsValid(killer) and killer:IsActive() then
+						if SIDEKICK and killer:GetSubRole() == ROLE_SIDEKICK then
+							killer = killer:GetSidekickMate() or nil
+						end
 
-				SendFullStateUpdate()
-			end)
+						if IsValid(killer) and killer:IsActive() then
+							p:UpdateRole(killer:GetSubRole(), killer:GetTeam())
+							p:SetDefaultCredits()
+
+							SendFullStateUpdate()
+						end
+					end
+				end)
+			end
 		end
 	end)
 end
