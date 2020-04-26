584c7e7ae56b9c12a467001a4784a90b28d3eefa
Alessandro Levante <62677560+PinoMartirio@users.noreply.github.com>
2020-04-26 14:56:05 +0200

    Added Italian translation (#2)
    * Added Italian translation

--- lua/terrortown/entities/roles/unknown/shared.lua
+++ lua/terrortown/entities/roles/unknown/shared.lua
@@ -36,6 +36,14 @@ function ROLE:Initialize()
 		LANG.AddToLanguage("English", "ttt2_desc_" .. self.name, [[The Unknown can decide (more or less) the role.
 Get killed by someone to copy the role of your killer!]])
 
+		LANG.AddToLanguage("Italiano", self.name, "Unknown")
+		LANG.AddToLanguage("Italiano", "info_popup_" .. self.name, [[Puoi decidere chi vuoi essere...]])
+		LANG.AddToLanguage("Italiano", "body_found_" .. self.abbr, "Era un Unknown...")
+		LANG.AddToLanguage("Italiano", "search_role_" .. self.abbr, "Questa persona era un Unknown!")
+		LANG.AddToLanguage("Italiano", "target_" .. self.name, "Unknown")
+		LANG.AddToLanguage("Italiano", "ttt2_desc_" .. self.name, [[L'Unknown può decidere (più o meno) il ruolo.
+Fatti uccidere da qualcuno per prendere il suo ruolo!]])
+
 		LANG.AddToLanguage("Deutsch", self.name, "Unbekannter")
 		LANG.AddToLanguage("Deutsch", "info_popup_" .. self.name, [[Du hast die Wahl, wer du sein willst...]])
 		LANG.AddToLanguage("Deutsch", "body_found_" .. self.abbr, "Er war ein Unbekannter...")
