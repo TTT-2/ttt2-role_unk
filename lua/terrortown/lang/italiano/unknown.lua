05c4b1f36bbeff645485d2dbac2dc93571c39954
ThePlatinumGhost <36534723+ThePlatinumGhost@users.noreply.github.com>
2020-10-09 17:40:28 +0200

    Update Italian

--- lua/terrortown/lang/italiano/unknown.lua
+++ lua/terrortown/lang/italiano/unknown.lua
@@ -1,13 +1,13 @@
 local L = LANG.GetLanguageTableReference("italiano")
 
 -- GENERAL ROLE LANGUAGE STRINGS
-L[UNKNOWN.name] = "Unknown"
+L[UNKNOWN.name] = "Sconosciuto"
 L["info_popup_" .. UNKNOWN.name] = [[Puoi decidere chi vuoi essere...
 Fatti uccidere da qualcuno per prendere il suo ruolo!]]
-L["body_found_" .. UNKNOWN.abbr] = "Era un Unknown..."
-L["search_role_" .. UNKNOWN.abbr] = "Questa persona era un Unknown!"
-L["target_" .. UNKNOWN.name] = "Unknown"
-L["ttt2_desc_" .. UNKNOWN.name] = [[L'Unknown può decidere (più o meno) il ruolo.
+L["body_found_" .. UNKNOWN.abbr] = "Era uno Sconosciuto..."
+L["search_role_" .. UNKNOWN.abbr] = "Questa persona era uno Sconosciuto!"
+L["target_" .. UNKNOWN.name] = "Sconosciuto"
+L["ttt2_desc_" .. UNKNOWN.name] = [[Lo Sconosciuto può decidere (più o meno) il ruolo.
 Fatti uccidere da qualcuno per prendere il suo ruolo!]]
 
 -- OTHER ROLE LANGUAGE STRINGS
