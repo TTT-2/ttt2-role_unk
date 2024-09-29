b4c91a582e1bdfe2ee7486e31837c381b255690c
MisterClems <73431903+MisterClems@users.noreply.github.com>
2024-09-30 01:46:23 +0200

    Update French translation

--- lua/terrortown/lang/fr/unknown.lua
+++ lua/terrortown/lang/fr/unknown.lua
@@ -3,9 +3,9 @@ local L = LANG.GetLanguageTableReference("fr")
 -- GENERAL ROLE LANGUAGE STRINGS
 L[UNKNOWN.name] = "Inconnu"
 L["info_popup_" .. UNKNOWN.name] = [[Vous pouvez décider qui vous voulez être...
-Faites-vous tuer par quelqu'un pour recevoir son rôle!]]
-L["body_found_" .. UNKNOWN.abbr] = "C'était un Inconnu."
-L["search_role_" .. UNKNOWN.abbr] = "Cette personne était un Inconnu!"
+Faites-vous tuer par quelqu'un pour recevoir son rôle !]]
+L["body_found_" .. UNKNOWN.abbr] = "C'était un Inconnu !"
+L["search_role_" .. UNKNOWN.abbr] = "C'était un Inconnu !"
 L["target_" .. UNKNOWN.name] = "Inconnu"
 L["ttt2_desc_" .. UNKNOWN.name] = [[L'Inconnu peut décider (plus ou moins) de son rôle.
 Faites-vous tuer par quelqu'un pour recevoir son rôle!]]
@@ -14,6 +14,6 @@ Faites-vous tuer par quelqu'un pour recevoir son rôle!]]
 L["unknown_revival"] = "Vous allez être réanimé!"
 L["unknown_revival_text"] = "Vous allez être réanimé en tant que {role} dans {time} seconde(s). Soyez prêt."
 L["unknown_revival_canceled"] = "Réanimation annulée"
-L["unknown_revival_canceled_text"] = "Votre réanimation a été annulée parce que votre tueur est mort prématurément."
+L["unknown_revival_canceled_text"] = "Votre réanimation a été annulée car votre meurtrier est mort prématurément."
 
---L["label_unknown_respawn_time"] = "Respawn time"
+L["label_unknown_respawn_time"] = "Temps de réanimation en secondes"
